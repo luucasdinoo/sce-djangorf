2d0a9f81f4821e8b57b4bc0563323487759ead26
Lucas Bernadino <luucasdinoo@gmail.com>
2024-09-27 15:42:12 -0300

    Crud básico e rápico com ModelViewSet

--- business/urls.py
+++ business/urls.py
@@ -1,6 +1,18 @@
-from django.urls import path
-from . import views
+# from django.urls import path
+# from . import views
+#
+# urlpatterns = [
+#     path('business/', views.BusinessCreateListView.as_view(), name='business-create-list'),
+#     path('business/<uuid:pk>/', views.BusinessRetrievelDestroyView.as_view(), name='business-detail-view'),
+# ]
+
+from django.urls import path, include
+from business import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('business', views.BusinnesViewSet, basename='business')
 
 urlpatterns = [
-    path('business/', views.BusinessCreateListView.as_view(), name='business-create-list'),
-]
+     path('', include(router.urls)),
+ ]
--- business/views.py
+++ business/views.py
@@ -1,6 +1,15 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from business.models import Business
 from business.serializers import BusinessSerializer
-class BusinessCreateListView(generics.ListCreateAPIView):
+
+class BusinnesViewSet(viewsets.ModelViewSet):
     queryset = Business.objects.all()
     serializer_class = BusinessSerializer
+
+# class BusinessCreateListView(generics.ListCreateAPIView):
+#     queryset = Business.objects.all()
+#     serializer_class = BusinessSerializer
+#
+# class BusinessRetrievelDestroyView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Business.objects.all()
+#     serializer_class = BusinessSerializer
